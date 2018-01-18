fb19cb5a7fa506e80906ad52157c604bef996fa2
wjl <wjl7123093@163.com>
2018-01-18 17:27:09 +0800

    SharePreference Key

---
+++ app/src/main/java/com/mytv/rtzhdj/app/SharepreferenceKey.java
@@ -0,0 +1,17 @@
+package com.mytv.rtzhdj.app;
+
+/**
+ * Sharepreference Keys
+ *
+ * @author Fred_W
+ * @version v1.0.0(1)
+ *
+ * @crdate 2018-1-18
+ * @update
+ */
+public interface SharepreferenceKey {
+
+    // Access_Token
+    String KEY_ACCESS_TOKEN = "access_token";
+
+}
