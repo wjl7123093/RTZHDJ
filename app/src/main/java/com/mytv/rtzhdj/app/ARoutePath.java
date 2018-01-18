0c1886a459067ab59ec68ad0a4e7d7cf691ccf21
wjl <wjl7123093@163.com>
2018-01-18 17:27:29 +0800

    ARoute Path

---
+++ app/src/main/java/com/mytv/rtzhdj/app/ARoutePath.java
@@ -0,0 +1,18 @@
+package com.mytv.rtzhdj.app;
+
+/**
+ * ARoute 路径
+ *
+ * @author Fred_W
+ * @version v1.0.0(1)
+ *
+ * @crdate 2018-1-18
+ * @update
+ */
+public interface ARoutePath {
+
+    // Main
+    String PATH_MAIN = "/rtdzdj/main";
+
+
+}
