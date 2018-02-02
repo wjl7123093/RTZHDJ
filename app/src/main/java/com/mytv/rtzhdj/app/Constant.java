a038ef3f8a0fb409d9c83c627069d2782fcf6141
wjl <wjl7123093@163.com>
2018-02-02 14:51:53 +0800

    新增 typeColumn 类型

--- app/src/main/java/com/mytv/rtzhdj/app/Constant.java
+++ app/src/main/java/com/mytv/rtzhdj/app/Constant.java
@@ -8,10 +8,13 @@ package com.mytv.rtzhdj.app;
  *
  * @crdate 2018-1-22
  * @update 2018-1-23    新增 typeImage, typeHeader 类型
+ *         2018-2-2     新增 typeColumn 类型
  */
 public class Constant {
 
     // RecyclerView item 类型
+    // 如果 item 布局不一致，那么 type 就必须不一致，否则就会导致 滑动卡顿
+    // e.g. 两个不同布局的 item，却使用同一个 type，那么就会导致 滑动卡顿
     public interface viewType{
         int typeBanner = 1;         //轮播图
         int typeGv = 2;             //九宫格
@@ -20,7 +23,7 @@ public class Constant {
         int typeNews = 5;           //新闻
         int typeMarquee = 6;        //跑马灯
         int typePlus = 7 ;          //不规则视图
-        int typeSticky = 8;         //指示器
+        int typeColumn = 8;         //格栏布局
         int typeFooter = 9;         //底部
         int typeGvSecond = 10;      //九宫格
         int typeImage = 11;         //图片
