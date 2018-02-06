8fb7d05dac8e268143cd6be95c03bfb38053ab60
wjl <wjl7123093@163.com>
2018-02-06 13:40:27 +0800

    填充 党员教育页 布局

--- app/src/main/java/com/mytv/rtzhdj/mvp/contract/NewsEducationContract.java
+++ app/src/main/java/com/mytv/rtzhdj/mvp/contract/NewsEducationContract.java
@@ -1,17 +1,39 @@
 package com.mytv.rtzhdj.mvp.contract;
 
+import android.support.v7.widget.RecyclerView;
+
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.jess.arms.mvp.IPresenter;
 import com.jess.arms.mvp.IView;
 import com.jess.arms.mvp.IModel;
+import com.mytv.rtzhdj.app.data.entity.NewsEntity;
+import com.mytv.rtzhdj.mvp.ui.activity.MainActivity;
+import com.mytv.rtzhdj.mvp.ui.activity.NewsEducationActivity;
+import com.mytv.rtzhdj.mvp.ui.adapter.BaseDelegateAdapter;
+
+import java.util.List;
 
 
 public interface NewsEducationContract {
     //对于经常使用的关于UI的方法可以定义到IView中,如显示隐藏进度条,和显示文字消息
     interface View extends IView {
+        void setOnGridClick(int position);
+        void setOnListClick(int position);
 
     }
 
     //Model层定义接口,外部只需关心Model返回的数据,无需关心内部细节,即是否使用缓存
     interface Model extends IModel {
 
+    }
+
+    //Presenter控制器
+    interface Presenter extends IPresenter {
+        void setActivity(NewsEducationActivity activity);
+        //初始化
+        DelegateAdapter initRecyclerView(RecyclerView recyclerView);
+        BaseDelegateAdapter initGvMenu();
+        BaseDelegateAdapter initTitle(String title);
+        BaseDelegateAdapter initList(List<NewsEntity> data);
     }
 }
--- app/src/main/java/com/mytv/rtzhdj/mvp/presenter/NewsEducationPresenter.java
+++ app/src/main/java/com/mytv/rtzhdj/mvp/presenter/NewsEducationPresenter.java
@@ -1,7 +1,18 @@
 package com.mytv.rtzhdj.mvp.presenter;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.res.TypedArray;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.alibaba.android.vlayout.layout.GridLayoutHelper;
+import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
+import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.jess.arms.integration.AppManager;
 import com.jess.arms.di.scope.ActivityScope;
 import com.jess.arms.mvp.BasePresenter;
@@ -11,15 +22,27 @@ import me.jessyan.rxerrorhandler.core.RxErrorHandler;
 
 import javax.inject.Inject;
 
+import com.jess.arms.utils.ArmsUtils;
+import com.mytv.rtzhdj.R;
+import com.mytv.rtzhdj.app.Constant;
+import com.mytv.rtzhdj.app.data.entity.NewsEntity;
 import com.mytv.rtzhdj.mvp.contract.NewsEducationContract;
+import com.mytv.rtzhdj.mvp.ui.activity.NewsEducationActivity;
+import com.mytv.rtzhdj.mvp.ui.adapter.BaseDelegateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @ActivityScope
-public class NewsEducationPresenter extends BasePresenter<NewsEducationContract.Model, NewsEducationContract.View> {
+public class NewsEducationPresenter extends BasePresenter<NewsEducationContract.Model, NewsEducationContract.View>
+    implements NewsEducationContract.Presenter {
     private RxErrorHandler mErrorHandler;
     private Application mApplication;
     private ImageLoader mImageLoader;
     private AppManager mAppManager;
+
+    private NewsEducationActivity mActivity;
 
     @Inject
     public NewsEducationPresenter(NewsEducationContract.Model model, NewsEducationContract.View rootView
@@ -41,4 +64,94 @@ public class NewsEducationPresenter extends BasePresenter<NewsEducationContract.
         this.mApplication = null;
     }
 
+    @Override
+    public void setActivity(NewsEducationActivity activity) {
+        mActivity = activity;
+    }
+
+    @Override
+    public DelegateAdapter initRecyclerView(RecyclerView recyclerView) {
+        //初始化
+        //创建VirtualLayoutManager对象
+        VirtualLayoutManager layoutManager = new VirtualLayoutManager(mActivity);
+//        layoutManager.setRecycleChildrenOnDetach(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //设置回收复用池大小，（如果一屏内相同类型的 View 个数比较多，需要设置一个合适的大小，防止来回滚动时重新创建 View）
+        RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
+        recyclerView.setRecycledViewPool(viewPool);
+        viewPool.setMaxRecycledViews(0, 20);
+
+        //设置适配器
+        DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager, true);
+        recyclerView.setAdapter(delegateAdapter);
+        return delegateAdapter;
+    }
+
+    @Override
+    public BaseDelegateAdapter initGvMenu() {
+        // 在构造函数设置每行的网格个数
+        final TypedArray proPic = mActivity.getResources().obtainTypedArray(R.array.education_gv_images);
+        final String[] proName = mActivity.getResources().getStringArray(R.array.education_gv_title);
+        final List<Integer> images = new ArrayList<>();
+        for(int a=0 ; a<proName.length ; a++){
+            images.add(proPic.getResourceId(a,R.mipmap.ic_launcher));
+        }
+        proPic.recycle();
+        GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(2);
+//        gridLayoutHelper.setPadding(0, 16, 0, 16);
+        gridLayoutHelper.setVGap(2);   // 控制子元素之间的垂直间距
+        gridLayoutHelper.setHGap(2);    // 控制子元素之间的水平间距
+//        gridLayoutHelper.setBgColor(Color.WHITE);
+//        gridLayoutHelper.setMarginBottom(ArmsUtils.dip2px(activity, 12));
+        return new BaseDelegateAdapter(mActivity, gridLayoutHelper, R.layout.item_vlayout_grid, 4, Constant.viewType.typeGv) {
+            @Override
+            public void onBindViewHolder(BaseViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+                super.onBindViewHolder(holder, position);
+                holder.setText(R.id.tv_name, proName[position]);
+                holder.setImageResource(R.id.iv_icon, images.get(position));
+                holder.getView(R.id.ll_container).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mRootView.setOnGridClick(position);
+                    }
+                });
+            }
+        };
+    }
+
+    @Override
+    public BaseDelegateAdapter initTitle(String title) {
+        SingleLayoutHelper singleLayoutHelper = new SingleLayoutHelper();
+        return new BaseDelegateAdapter(mActivity, singleLayoutHelper , R.layout.item_vlayout_title,
+                1, Constant.viewType.typeTitle) {
+            @Override
+            public void onBindViewHolder(BaseViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                holder.setGone(R.id.tv_moredata, false);
+                holder.setText(R.id.tv_title, title);
+            }
+        };
+    }
+
+    @Override
+    public BaseDelegateAdapter initList(List<NewsEntity> data) {
+        LinearLayoutHelper linearLayoutHelper = new LinearLayoutHelper();
+        linearLayoutHelper.setDividerHeight(ArmsUtils.dip2px(mActivity, 1));
+        return new BaseDelegateAdapter(mActivity, linearLayoutHelper , R.layout.item_vlayout_list_image,
+                data.size(), Constant.viewType.typeList) {
+            @Override
+            public void onBindViewHolder(BaseViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                holder.setText(R.id.tv_title, data.get(position).getTitle());
+                holder.setText(R.id.tv_datetime, data.get(position).getDatetime());
+                holder.setText(R.id.tv_star_num, data.get(position).getStar_num() + "");
+                holder.setText(R.id.tv_comment_num, data.get(position).getComment_num() + "");
+
+                com.mytv.rtzhdj.app.utils.ImageLoader.getInstance().showImage(mActivity,
+                        holder.getView(R.id.iv_image), data.get(position).getImg_url());
+
+            }
+        };
+    }
 }
