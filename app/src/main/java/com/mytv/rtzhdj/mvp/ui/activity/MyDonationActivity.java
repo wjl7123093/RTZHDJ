833e55d9092993d549c25cdfd59c2f24f5a1d42e
wjl <wjl7123093@163.com>
2018-02-08 14:59:59 +0800

    填充 我的捐赠/所有捐赠 页面布局

--- app/src/main/java/com/mytv/rtzhdj/mvp/ui/activity/MyDonationActivity.java
+++ app/src/main/java/com/mytv/rtzhdj/mvp/ui/activity/MyDonationActivity.java
@@ -3,8 +3,17 @@ package com.mytv.rtzhdj.mvp.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
@@ -16,21 +25,49 @@ import com.mytv.rtzhdj.mvp.contract.MyDonationContract;
 import com.mytv.rtzhdj.mvp.presenter.MyDonationPresenter;
 
 import com.mytv.rtzhdj.R;
+import com.mytv.rtzhdj.mvp.ui.fragment.ContentFragment;
+import com.mytv.rtzhdj.mvp.ui.fragment.VolunteerServiceFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import me.weyye.library.colortrackview.ColorTrackTabLayout;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 /**
- * 我的捐赠界面
+ * 我的捐赠/所有捐赠 界面
  *
  * @author Fred_W
  * @version v1.0.0(1)
  *
  * @crdate 2018-1-21
- * @update
+ * @update 2018-2-8     填充布局
  */
 @Route(path = ARoutePath.PATH_MY_DONATION)
 public class MyDonationActivity extends BaseActivity<MyDonationPresenter> implements MyDonationContract.View {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.toolbar_title)
+    TextView mTvToolbarTitle;
+    @BindView(R.id.toolbar_back)
+    RelativeLayout mBtnToolbarBack;
+    @BindView(R.id.toolbar_menu)
+    RelativeLayout mBtnToolbarMenu;
+    @BindView(R.id.iv_menu)
+    ImageView mIvMenu;
+
+    @BindView(R.id.tab_channel)
+    ColorTrackTabLayout mTab;
+    @BindView(R.id.vp_content)
+    ViewPager mViewPager;
+
+    @Autowired
+    String type;
+
+    String[] titles;
 
 
     @Override
@@ -45,12 +82,20 @@ public class MyDonationActivity extends BaseActivity<MyDonationPresenter> implem
 
     @Override
     public int initView(Bundle savedInstanceState) {
+        ARouter.getInstance().inject(this);
         return R.layout.activity_my_donation; //如果你不需要框架帮你设置 setContentView(id) 需要自行设置,请返回 0
     }
 
     @Override
     public void initData(Bundle savedInstanceState) {
+        mIvMenu.setImageResource(R.mipmap.ic_launcher);
+        mBtnToolbarMenu.setOnClickListener(view -> {
+            // 跳转到搜索页面
+            ARouter.getInstance().build(ARoutePath.PATH_SEARCH).navigation();
+        });
 
+        titles = new String[]{"全部", "已被领取", "未被领取"};
+        initTab();
     }
 
 
@@ -81,5 +126,39 @@ public class MyDonationActivity extends BaseActivity<MyDonationPresenter> implem
         finish();
     }
 
+    private void initTab() {
+        final List<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            fragments.add(ContentFragment.newInstance(i));
+        }
 
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return titles.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles[position];
+            }
+        });
+        mTab.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (type.equals("all")) {
+            mTvToolbarTitle.setText(getResources().getString(R.string.title_all_donation));
+        } else {
+            mTvToolbarTitle.setText(getResources().getString(R.string.title_my_donation));
+        }
+    }
 }
