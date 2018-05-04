83c46150690a336db2a19809db7e8fb4ac060e77
wjl <wjl7123093@163.com>
2018-05-04 18:18:36 +0800

    优化 我要捐赠 页面UI

---
+++ app/src/main/java/com/mytv/rtzhdj/app/utils/RouteSystemUIUtils.java
@@ -0,0 +1,73 @@
+package com.mytv.rtzhdj.app.utils;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import com.mytv.rtzhdj.mvp.ui.activity.MyJoinActivity;
+
+import java.io.File;
+
+/**
+ * 跳转到 系统界面
+ * Created by wangjl on 2018/5/4.
+ */
+public class RouteSystemUIUtils {
+
+    /**
+     * 拍照
+     *
+     * @param path 照片存放的路径
+     */
+    public static void captureImage(Activity activity, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            doTakePhotoIn7(new File(Environment.getExternalStorageDirectory(), "image.jpg").getAbsolutePath(),
+                    activity, requestCode);
+        } else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "image.jpg")));
+            activity.startActivityForResult(intent, requestCode);
+        }
+    }
+
+    /**
+     * 从图库中选取图片
+     */
+    public static void selectImage(Activity activity, int requestCode) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        activity.startActivityForResult(intent, requestCode);
+    }
+
+    //在Android7.0以上拍照
+    private static void doTakePhotoIn7(String path, Activity activity, int requestCode) {
+        Uri mCameraTempUri;
+        try {
+            ContentValues values = new ContentValues(1);
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+            values.put(MediaStore.Images.Media.DATA, path);
+            mCameraTempUri = activity.getContentResolver()
+                    .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            takePhoto(requestCode, mCameraTempUri, activity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void takePhoto(int requestCode, Uri uri, Activity activity) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        if (uri != null) {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        }
+        activity.startActivityForResult(intent, requestCode);
+    }
+
+}
--- app/src/main/java/com/mytv/rtzhdj/mvp/ui/activity/DonateActivity.java
+++ app/src/main/java/com/mytv/rtzhdj/mvp/ui/activity/DonateActivity.java
@@ -1,10 +1,20 @@
 package com.mytv.rtzhdj.mvp.ui.activity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,13 +24,25 @@ import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 
 import com.mytv.rtzhdj.app.ARoutePath;
+import com.mytv.rtzhdj.app.utils.FileUtils;
+import com.mytv.rtzhdj.app.utils.ImageTools;
+import com.mytv.rtzhdj.app.utils.RouteSystemUIUtils;
 import com.mytv.rtzhdj.di.component.DaggerDonateComponent;
 import com.mytv.rtzhdj.di.module.DonateModule;
 import com.mytv.rtzhdj.mvp.contract.DonateContract;
 import com.mytv.rtzhdj.mvp.presenter.DonatePresenter;
 
 import com.mytv.rtzhdj.R;
+import com.mytv.rtzhdj.mvp.ui.adapter.GridViewAdapter;
+import com.mytv.rtzhdj.mvp.ui.widget.BottomDialog;
 
+
+import net.qiujuer.genius.ui.widget.Button;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -47,6 +69,36 @@ public class DonateActivity extends BaseActivity<DonatePresenter> implements Don
     @BindView(R.id.toolbar_menu)
     RelativeLayout mBtnToolbarMenu;
 
+    @BindView(R.id.edt_donate_theme)
+    EditText mEdtDonateTheme;
+    @BindView(R.id.edt_phone)
+    EditText mEdtPhone;
+    @BindView(R.id.edt_donate_content)
+    EditText mEdtDonateContent;
+    @BindView(R.id.gv_publish)
+    GridView mGvPublish;
+    @BindView(R.id.btn_donate)
+    Button mBtnDonate;
+
+    // 图片 九宫格适配器
+    private GridViewAdapter gvAdapter;
+    private BottomDialog mBottomDialog;
+    /** 是否选择照片上传 */
+    private boolean isSelectedPhoto = false;
+    // 拍照
+    public static final int IMAGE_CAPTURE = 1;
+    // 从相册选择
+    public static final int IMAGE_SELECT = 2;
+    // 照片缩小比例
+    private static final int SCALE = 5;
+    // 用于保存图片资源文件
+    private List<Bitmap> lists = new ArrayList<Bitmap>();
+    private List<String> list_pic = new ArrayList<String>();
+    // 用于保存图片路径
+    private List<String> list_path = new ArrayList<String>();
+    private String uploadPicture = "";
+    private String uploadPictureFromList = "";
+
 
     @Override
     public void setupActivityComponent(AppComponent appComponent) {
@@ -67,6 +119,25 @@ public class DonateActivity extends BaseActivity<DonatePresenter> implements Don
     public void initData(Bundle savedInstanceState) {
         mBtnToolbarMenu.setVisibility(View.GONE);
 
+        mBottomDialog = new BottomDialog(DonateActivity.this, R.layout.dialog_publish,
+                new int[]{R.id.tvCamera, R.id.tvAlbum, R.id.tvCancel});
+        initDialog();
+
+        gvAdapter = new GridViewAdapter(this, lists,list_pic, 0);
+        mGvPublish.setAdapter(gvAdapter);
+        gvAdapter.setList(lists, false);
+        mGvPublish.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == lists.size()) {
+                    // 弹出对话框
+                    mBottomDialog.show();
+                }
+
+            }
+        });
     }
 
 
@@ -97,5 +168,122 @@ public class DonateActivity extends BaseActivity<DonatePresenter> implements Don
         finish();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && resultCode != RESULT_CANCELED) {	// 拍照/相册
+
+            String fileName;
+            switch (requestCode) {
+                case IMAGE_CAPTURE:// 拍照返回
+                    // 将保存在本地的图片取出并缩小后显示在界面上
+                    Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/image.jpg");
+                    Bitmap newBitmap = ImageTools.zoomBitmap(bitmap,bitmap.getWidth() / SCALE, bitmap.getHeight() / SCALE);
+                    // 由于Bitmap内存占用较大，这里需要回收内存，否则会报out of memory异常
+                    bitmap.recycle();
+                    // 生成一个图片文件名
+                    fileName = String.valueOf(System.currentTimeMillis());
+                    // 将处理过的图片添加到缩略图列表并保存到本地
+                    ImageTools.savePhotoToSDCard(newBitmap, FileUtils.SDPATH,fileName);
+                    lists.add(newBitmap);
+                    list_path.add(fileName+".jpg");
+
+                    // 上传图片流
+                    try {
+//                        uploadPicture += Base64Utils.encodeFile(FileUtils.SDPATH + fileName + ".jpg") + ",";
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    for (int i = 0; i < list_path.size(); i++) {
+                        Log.i("UPLOAD", "第"+i+"张照片的地址："+list_path.get(i));
+                    }
+
+                    // 更新GrideView
+                    gvAdapter.setList(lists, false);
+
+                    // 选择照片后，标记
+                    isSelectedPhoto = true;
+                    break;
+
+                case IMAGE_SELECT:// 选择照片返回
+                    ContentResolver resolver = getContentResolver();
+                    // 照片的原始资源地址
+                    Uri originalUri = data.getData();
+                    try {
+                        // 使用ContentProvider通过URI获取原始图片
+                        Bitmap photo = MediaStore.Images.Media.getBitmap(resolver,originalUri);
+                        if (photo != null) {
+                            // 为防止原始图片过大导致内存溢出，这里先缩小原图显示，然后释放原始Bitmap占用的内存
+                            Bitmap smallBitmap = ImageTools.zoomBitmap(photo,photo.getWidth() / SCALE, photo.getHeight()/ SCALE);
+                            // 释放原始图片占用的内存，防止out of memory异常发生
+                            photo.recycle();
+                            // 生成一个图片文件名
+                            fileName = String.valueOf(System.currentTimeMillis());
+                            // 将处理过的图片添加到缩略图列表并保存到本地
+                            ImageTools.savePhotoToSDCard(smallBitmap, FileUtils.SDPATH,fileName);
+                            lists.add(smallBitmap);
+                            list_path.add(fileName+".jpg");
+
+                            // 上传图片流
+                            try {
+//                                uploadPicture += Base64Utils.encodeFile(FileUtils.SDPATH + fileName + ".jpg") + ",";
+                            } catch (Exception e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+
+                            for (int i = 0; i < list_path.size(); i++) {
+                                Log.i("UPLOAD", "第"+i+"照片的地址："+list_path.get(i));
+                            }
+
+                            // 更新GrideView
+                            gvAdapter.setList(lists, false);
+
+                            // 选择照片后，标记
+                            isSelectedPhoto = true;
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    /**
+     * 初始化对话框
+     */
+    private void initDialog() {
+        mBottomDialog.setOnBottomMenuItemClickListener(new BottomDialog.OnBottomMenuItemClickListener() {
+
+            @Override
+            public void onBottomMenuItemClick(BottomDialog dialog, View view) {
+                switch (view.getId()) {
+                    case R.id.tvCamera:		// 拍照
+                        RouteSystemUIUtils.captureImage(DonateActivity.this, IMAGE_CAPTURE);
+                        break;
+                    case R.id.tvAlbum:		// 从相册中选取
+                        RouteSystemUIUtils.selectImage(DonateActivity.this, IMAGE_SELECT);
+                        break;
+                    case R.id.tvCancel:		// 取消
+                        dialog.dismiss();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        });
+    }
 
 }
